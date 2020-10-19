05246ae18376d56062bf5e75acac5648ff1a65c3
dayam <dayam@Xs-MacBook-Air.local>
2020-10-19 17:01:47 -0600

    Removed --force-samples flag

--- clean_vcf.sh
+++ clean_vcf.sh
@@ -4,6 +4,6 @@ in_vcf=$1
 sample_file=$2
 out_vcf=`basename $in_vcf | sed 's/.vcf.gz/_clean.vcf.gz/'`
 
-bcftools view --samples-file $sample_file --force-samples --min-ac=1 --apply-filters PASS --no-update $in_vcf | bgzip -c > $out_vcf
+bcftools view --samples-file $sample_file --min-ac=1 --apply-filters PASS --no-update $in_vcf | bgzip -c > $out_vcf
 tabix $out_vcf
 
