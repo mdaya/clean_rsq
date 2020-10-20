33781fcca8037d82baddb155abd5d90d2b612cab
dayam <dayam@Xs-MacBook-Air.local>
2020-10-20 09:48:13 -0600

    Corrected typo

--- bgzip_vcf.sh
+++ bgzip_vcf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 in_gz_vcf=$1
-out_gz_vcf=`basename $in_vcf | sed 's/.vcf.gz/_bgzipped.vcf.gz/'`
+out_gz_vcf=`basename $in_gz_vcf | sed 's/.vcf.gz/_bgzipped.vcf.gz/'`
 
 zcat $in_gz_vcf | bgzip -c > $out_gz_vcf
 tabix $out_gz_vcf
