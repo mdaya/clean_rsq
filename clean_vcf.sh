04ea57d97740030e3067a726ad696aa7fec204fd
dayam <dayam@Xs-MacBook-Air.local>
2020-10-26 11:44:37 -0600

    Seperated sample filter from allele filter step so as to exclude non-variant alleles

--- clean_vcf.sh
+++ clean_vcf.sh
@@ -4,6 +4,10 @@ in_vcf=$1
 sample_file=$2
 out_vcf=`basename $in_vcf | sed 's/.vcf.gz/_clean.vcf.gz/'`
 
-bcftools view --samples-file $sample_file --min-ac=1 --apply-filters PASS --no-update $in_vcf | bgzip -c > $out_vcf
+tmp_sample_vcf=tmp_samples_${out_vcf}
+bcftools view --samples-file $sample_file $in_vcf | bgzip -c > $tmp_sample_vcf
+tabix $tmp_sample_vcf
+
+bcftools view --min-ac=1 --apply-filters PASS --no-update $tmp_sample_vcf | bgzip -c > $out_vcf
 tabix $out_vcf
 
