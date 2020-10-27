b58e9f789e1a57c09c6a4abdb5adfd70856f07ac
dayam <dayam@Xs-MacBook-Air.local>
2020-10-27 08:59:44 -0600

    Added :minor option to --min-ac to filter out by minor rather than alternate allele

--- clean_vcf.sh
+++ clean_vcf.sh
@@ -3,11 +3,6 @@
 in_vcf=$1
 sample_file=$2
 out_vcf=`basename $in_vcf | sed 's/.vcf.gz/_clean.vcf.gz/'`
-tmp_sample_vcf=`basename $in_vcf | sed 's/.vcf.gz/_samples.vcf.gz/'`
 
-bcftools view --samples-file $sample_file $in_vcf | bgzip -c > $tmp_sample_vcf
-tabix $tmp_sample_vcf
-
-bcftools view --min-ac=1 --apply-filters PASS --no-update $tmp_sample_vcf | bgzip -c > $out_vcf
+bcftools view --samples-file $sample_file $in_vcf --min-ac=1:minor --apply-filters PASS | bgzip -c > $out_vcf
 tabix $out_vcf
-
