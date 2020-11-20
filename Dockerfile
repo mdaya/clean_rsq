129af45ac5fa8570c33473ca2c5acdad753d853d
dayam <dayam@Xs-MacBook-Air.local>
2020-11-20 16:46:00 -0700

    Added vcftools to docker image and changed output name

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM images.sbgenomics.com/luka_topalovic/bcftools:1.9
 
+#Install VCFTools
+RUN apt-get update -y && apt-get install -y \
+    vcftools 
+
 # Install scripts
 RUN mkdir /home/analyst
 COPY *_vcf.sh /home/analyst/
--- clean_missing_geno_vcf.sh
+++ clean_missing_geno_vcf.sh
@@ -2,7 +2,7 @@
 
 in_vcf=$1
 max_missing=$2
-out_vcf=`basename $in_vcf | sed 's/.vcf.gz/_clean_missing_geno.vcf.gz/'`
+out_vcf=`basename $in_vcf | sed 's/.vcf.gz/_missing_geno.vcf.gz/'`
 
 vcftools --gzvcf $in_vcf --max-missing $max_missing --recode --stdout | bgzip -c > $out_vcf
 tabix $out_vcf
